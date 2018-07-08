d2bf99eaf6bbb5262ed445287737c53c47a2269f
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2018-07-08 17:24:25 -0300

    [Unit Tests] Finished Task tests

---
+++ src/test/java/ar/edu/utn/frba/dds/group5/students/model/TaskTest.java
@@ -0,0 +1,37 @@
+package ar.edu.utn.frba.dds.group5.students.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TaskTest {
+    private Task task;
+
+    @Before
+    public void loadTestDependencies() {
+        task = new Task("Diseño de sistemas", "Tarea", true);
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals("Diseño de sistemas", task.getAssignature());
+        assertEquals("Tarea", task.getDescription());
+        assertEquals(true, task.isConceptual());
+    }
+
+    @Test
+    public void testSetters() {
+        task.setAssignature("Gestión de datos");
+        task.setDescription("TP cuatrimestral");
+        task.setConceptual(false);
+        assertEquals("Gestión de datos", task.getAssignature());
+        assertEquals("TP cuatrimestral", task.getDescription());
+        assertEquals(false, task.isConceptual());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Diseño de sistemas - Tarea", task.toString());
+    }
+}
